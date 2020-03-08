6f4ef0a844cc8d17c76ebb9627cdff00a42a93ad
sijeong <sijeong@outlook.com>
2020-03-08 23:44:20 +0900

    minor changes for ...

--- src/Home.tsx
+++ src/Home.tsx
@@ -13,30 +13,11 @@ class Home extends React.Component {
           <div className="Home-header">
             <img src={logo} className="Home-logo" alt="logo" />
             <h2>Welcome to Razzle</h2>
+            <TodoListOperations />
+            <TodoList />
           </div>
-          <TodoListOperations />
-          <TodoList />
-          <p className="Home-intro">
-            To get started, edit <code>src/App.js</code> or{' '}
-            <code>src/Home.js</code> and save to reload.
-        </p>
-          <ul className="Home-resources">
-            <li>
-              <a href="https://github.com/jaredpalmer/razzle">Docs</a>
-            </li>
-            <li>
-              <a href="https://github.com/jaredpalmer/razzle/issues">Issues</a>
-            </li>
-            <li>
-              <a href="https://palmer.chat">Community Slack</a>
-            </li>
-            <li>
-              <a href="https://www.google.com">added later</a>
-            </li>
-          </ul>
         </div>
       </Provider>
-
     );
   }
 }
